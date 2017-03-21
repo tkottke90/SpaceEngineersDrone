898fea8d1951738bebaa23e6a34237da1876d3b1
tkottke90 <altotom90@gmail.com>
2017-03-21 09:21:19 -0500

    Work on Boilerplate
    ** Changes **
    - Added code to force used to name LCD Panel 'LCDStatus' as the main LCD
    Panel on the Grid
    
    ** Issues **
    
    ** To-Do **
    - Continue Boilerplate

--- Drone Script v1.cs
+++ Drone Script v1.cs
@@ -1,9 +1,21 @@
+/*
+	Instructions:
+		1) Add LCD Panel and call it LCDStatus
+
+
+
+
+*/
+
 // Variables
 IMyRemoteControl remote;
-IMyTextPanel[] lcdMain;
+IMyTextPanel lcdMain;
+IMyTextPanel lcdPanels;
 
+boolean compSuccess = true;
+integer sessionCount = 0;
 string terminalData;
-
+string newTerminalData;
 
 public void Main(string argument){
 	// Initialize Variables
@@ -15,16 +27,26 @@ public void Main(string argument){
 		GridTerminalSystem.GetBlocksOfType<IMyRemoteControl>(list0);
 		remote = (IMyRemoteControl) list0[0];
 		// Main LCD:
+		lcdMain = (IMyTextPanel) GridTerminalSystem.GetBlocksWithName("LCDStatus");
+		if(lcdMain == null){
+			newTerminalData += "Error: Missing LCDStatus - \r\n Please Add LCD Panel Named LCDStatus\r\n";
+			Echo("Error Occured - See Custom Data");
+			compSuccess = false
+		}
+		// Other LCDs
 		GridTerminalSystem.GetBlocksOfType<IMyTextPanel>(list0);
-		for(int i = 0; i < list0.count;i++){lcdMain.add((IMyTextPanel)list0[i]);}
+		for(int i = 0; i < list0.count;i++){lcdPanels.add((IMyTextPanel)list0[i]);}
 		
+	// Extract terminalData
+
+
+	// End of Run
+	writeToLCD()
 }
 
 
 public void setAutoPilot(Vector3D coord){
 	
-	string[] data = {};
-	data.add("test");
 	
 }
 

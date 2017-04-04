a9a27f183d82cf4951a083fdd3f250fd6b92c441
tkottke90 <altotom90@gmail.com>
2017-04-04 09:20:08 -0500

    Added Preferences to CustomData and GPS Recorder Sample file

--- Drone Script v1.cs
+++ Drone Script v1.cs
@@ -1,98 +1,110 @@
-/*
-	Instructions:
-		1) Add LCD Panel and call it LCDStatus
+ 
+// Coniguration: 
+int DefaultRadius = 5000; 
+MyWaypointInfo originGPS; 
+string OriginType; 
+string OriginComm; 
+ 
+// Variables  
+    // Utility 
+    IMyRemoteControl remote;  
+    IMyTextPanel lcdMain; 
+    List<IMyTextPanel> lcds = new List<IMyTextPanel>();  
+    string terminalData;  
+    string newTerminalData; 
+ 
+    // Navigation 
+    Vector3D patrolCoord; 
+    Vector3D targetCoord; 
+         
+ 
+ 
+public void Main(string argument){  
+    	// Initialize Variables  
+	   List<IMyTerminalBlock> list0 = new List<IMyTerminalBlock>();  
+	   terminalData = Storage;    
+	  
+    	// Set Variables  
+		    // Remote Control:  
+	   	 GridTerminalSystem.GetBlocksOfType<IMyRemoteControl>(list0);  
+		    remote = (IMyRemoteControl) list0[0];  
+		    // Main LCD:  
+        lcdMain = (IMyTextPanel)GridTerminalSystem.GetBlockWithName("LCDStatus"); 
+        if(lcdMain == null){newTerminalData +=  
+                "Error: Missing LCDStatus - \r\n Please Add LCD Panel Named LCDStatus\r\n";} 
+        writeToLCD(lcdMain,"",false); 
+        // Other LCDs: 
+     
+    getPreferences();   
+
+    writeToLCD(lcdMain, "Waypoints: \r\n", true); 
+    if(lcdMain != null){ 
+        List<MyWaypointInfo> waypoints = new List<MyWaypointInfo>();  
+        remote.GetWaypointInfo(waypoints);  
+		 
+        MyWaypointInfo temp = genNewCoord();  
+        waypoints.Add(temp);  
+        remote.AddWaypoint(temp.Coords,temp.Name);         
+    }else{Echo("LCDMain = Null");} 
+ 
+} 
+ 
+public void getPreferences(){ 
+    string pref = Me.CustomData;
+    string[] prefs = pref.Split('\n');
+
+    for(int i = 1; i < prefs.Length; i++){
+        writeToLCD(lcdMain,(i + ")" + prefs[i] + "\n"),true);
+    }
+
+    // Default Radius
+    DefaultRadius = Int32.Parse(prefs[1].Split(':')[1]);
+    // Origin Type
+    OriginType = prefs[2].Split(':')[1];
+    // Origin Comm
+    OriginComm = prefs[3].Split(':')[1];
+    // Origin GPS
+    if(prefs[4].Split(':')[1] != ""){
+        originGPS = recoverGPS(prefs[4].Split(':')[1]);
+    }else{
+        originGPS = remote.
+    }
+} 
+ 
+public MyWaypointInfo recoverGPS(string waypoint){ 
+    writeToLCD(lcdMain, waypoint, false);    
 
 
-
-
-*/
-
-// Configuration:										
-
-const DefaultRadius = 5000;
-
-// Variables
-IMyRemoteControl remote;
-IMyTextPanel lcdMain;
-IMyTextPanel lcdPanels;
-
-boolean compSuccess = true;
-integer sessionCount = 0;
-string terminalData;
-string log;
-
-string status = "";
-
-public void Main(string argument){
-	// Initialize Variables
-	List<IMyTerminalBlock> list0 = new List<IMyTerminalBlock>();
-	terminalData = Me.CustomData;
-	
-	// Set Variables
-		// Remote Control:
-		GridTerminalSystem.GetBlocksOfType<IMyRemoteControl>(list0);
-		remote = (IMyRemoteControl) list0[0];
-		// Main LCD:
-		lcdMain = (IMyTextPanel) GridTerminalSystem.GetBlocksWithName("LCDStatus");
-		if(lcdMain == null){
-			addErrorMessage("Error: Missing LCDStatus - \r\n Please Add LCD Panel Named LCDStatus\r\n")
-			compSuccess = false
-		}
-		// Other LCDs
-		GridTerminalSystem.GetBlocksOfType<IMyTextPanel>(list0);
-		for(int i = 0; i < list0.count;i++){lcdPanels.add((IMyTextPanel)list0[i]);}
-		
-	// Extract terminalData
-
-	List<MyWaypointInfo> waypoints = new List<MyWaypointInfo>(); 
-	remote.GetWaypointInfo(waypoints); 
-	for(int i = 0; i < waypoints.Count; i++){ 
-		string name = waypoints[i].Name; 
-		Vector3D coords = waypoints[i].Coords; 
-		writeToLCD(lcdMain,(i + ")" + name + ":\r\n"),true); 
-		writeToLCD(lcdMain,(coords.ToString() + "\r\n"),true); 
-	}
-
-
-	// End of Run
-		// Write Any Error Messages
-		if(log.length > 0){writeToLCD(lcdMain,log,true);}
-		// Update Drone Data
-}
-
-/*
-	Terminal Data:
-	- Origin
-	- Sesson Count 
-	- 
-
-
- */
-
-public MyWaypointInfo genNewCoord(){
-    Random rnd = new Random();
-
-    int x = rnd.Next(1,DefaultRadius);
-    int y = rnd.Next(1,DefaultRadius);
-    int z = rnd.Next(1,DefaultRadius);
-    
-    Vector3D coord = new Vector3D(x,y,z);
-
-    return new MyWaypointInfo("testCoord",coord);
-}
-
-public void addErrorMessage(string error){
-	log += error + "\r\n";
-	Echo("Error Occured - See Custom Data");
-}
-
-public void setAutoPilot(Vector3D coord){
-	
-	
-}
-
-public void writeToLCD(IMyTextPanel lcd, string output, bool append){
-	// Applys text to LCD Screens
-	((IMyTextPanel)lcd).WritePublicText(output,append);
-	((IMyTextPanel)lcd).ShowPublicTextOnScreen();
+    return new MyWaypointInfo(); 
+} 
+ 
+public void writeToLCD(IMyTextPanel lcd, string output, bool append){  
+    	// Applys text to LCD Screens  
+    	((IMyTextPanel)lcd).WritePublicText(output,append);  
+	   ((IMyTextPanel)lcd).ShowPublicTextOnScreen();  
+} 
+ 
+public MyWaypointInfo genNewCoord(){ 
+    Random rnd = new Random(); 
+ 
+    int x = 0; //genRandomNumber(); 
+    int y = 0; //genRandomNumber(); 
+    int z = 0; //genRandomNumber(); 
+     
+    Vector3D coord = new Vector3D(x,y,z); 
+ 
+    return new MyWaypointInfo("testCoord",coord); 
+} 
+ 
+public int genRandomNumber(){ 
+    Random rnd = new Random(); 
+     
+    int direction = 1; 
+    int number = rnd.Next(1,DefaultRadius); 
+ 
+    if(direction == 1){ 
+        return number; 
+    }else{ 
+        return (number * -1); 
+    } 
 }
